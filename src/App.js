dfb0ccf4e5fd46e2ace8179adfcc0d358ec6aa9a
Maria Luisa <malu.olicosta@gmail.com>
2020-09-24 00:25:06 -0300

    Página de registros

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
-import Landing from './pages/landing'
+import Landing from './pages/landing';
+import Register from './pages/register';
 
 import './assets/global.css';
 
 function App() {
   return (
     <Landing/>
+    <Register/>
   );
 }
 
---
+++ src/pages/register/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './style.css';
+import branchIcon from "../../assets/img/register/icone-galho.svg";
+import heartIcon from "../../assets/img/register/icone-coracao.png";
+
+const Register = () => {
+    return (
+        <section className="register-container">            
+            <div className="register-user">
+                
+                <img className="icon-register" src={branchIcon} alt="Ícone de galho com folhas."/>   
+
+                <p>Para você que tem interesse em fazer doação
+                    ou ser voluntário em alguma causa social.
+                </p>
+
+                <button className="btn-register btn-user">Cadastre-se</button>
+
+            </div>
+
+            <div className="register-idea">
+                
+                <img className="icon-register" src={heartIcon} alt="Ícone de coração."/>
+
+                <p>Para você que quer ajudar em alguma causa social e precisa de doações ou trabalhos voluntários.
+                </p>
+                
+                <button className="btn-register btn-idea">Cadastrar sua ideia</button>
+            </div>
+
+        </section>
+    )
+}
+
+export default Register;
