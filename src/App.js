38f5efcbad371c428a81b011df2fc24067d47782
Ana Gisely <anagisely.silva.alves@gmail.com>
2020-10-14 21:05:06 -0300

    adição do componente footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react'; 
-//import Routes from './services/routes'
+import Routes from './services/routes'
 
 import './assets/global.css';
-import Footer from './pages/Footer';
-import RegisterIdeias from './pages/RegisterIdeia';
+
 
 
 
 function App() {
   return (
-    
+    <Routes/>
     
   );
 }
