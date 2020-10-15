e12a9a594b113de205d5dd7e3b21777b136ac950
tiagoaugustoFront@gmail.com <tiagoaugustoFront@gmail.com>
2020-10-15 19:24:37 -0300

    footer concluido e header faltando 70%

--- src/pages/Footer/index.js
+++ src/pages/Footer/index.js
@@ -5,38 +5,51 @@ import Logo from "../../assets/img/landing/Icon-Hoding-hands.svg";
 
 const Footer = () => {
     return(
-        <footer className="divFooter">
-            <div className="divLogo">
-                <div className="logo-footer">
-                    <img className="holding" src={Logo} alt="Logo da Holding Hands"/>
-                </div>
-                
-                <div className="logo-nome">
+    <footer className="footer-conatiner-all">
+        <div className="footer-conatiner">
+            <div className="logo-footer">
+                <img className="logo-footer-img" src={Logo} alt="Logo da Holding Hands"/>
+
+                <p className="logo-footer-title">
                     Holding Hands
+                </p>
+            </div>
+
+            <div className="footer-devs-all">
+                <div className="footer-devs">
+                    <li>
+                        <a className="footer-devs-itens" href="https://github.com/Anagisely">
+                        Ana Gisely
+                        </a>
+                    </li>
+
+                    <li>
+                        <a className="footer-devs-itens" href="https://github.com/aryane-cristine">
+                        Aryane Cristina
+                        </a>
+                    </li>
+                </div>
+
+                <div className="footer-devs">
+                    <li>
+                        <a className="footer-devs-itens" href="https://github.com/maluhcosta">
+                        Maria Luísa
+                        </a>
+                    </li>
+
+                    <li>
+                        <a className="footer-devs-itens" href="https://github.com/TiagoAugustoFront">
+                        Tiago Augusto
+                        </a>
+                    </li>
                 </div>
             </div>
             
-            <div className="copy">
-                <p> 
-                    &copy; Holding Hands 2020, 
-                    <a href="https://github.com/Anagisely">
-                        Ana Gisely,
-                    </a>
-                        
-                    <a href="https://github.com/aryane-cristine">
-                        Aryane Cristina,
-                    </a>
-                    
-                    <a href="https://github.com/maluhcosta">
-                         Maria Luísa,
-                    </a>
-                                
-                    <a href="https://github.com/TiagoAugustoFront">
-                        Tiago Augusto
-                    </a>.
-                </p>
-            </div>
-        </footer>
+            <p className="copy">
+                &copy; Holding Hands 2020
+            </p>
+        </div>
+    </footer>
     )    
 }
 
--- src/pages/Header/index.js
+++ src/pages/Header/index.js
@@ -1,30 +1,37 @@
 import React from  "react";
-import Logo from "../../assets/img/landing/logo-Hoding-Hands.svg";
+import Logo from "../../assets/img/landing/Icon-Hoding-hands.svg";
 import { Link } from "react-router-dom";
 
 import "./header.css";
 
 const Header = () => {
     return(
-        <header class="header">   
+        <header class="header">
             <div className='header-content'>
+
                 <Link to="/">
-                    <img src={Logo} alt="Logo holdin hands" class="logo-header"/>
+                    <div className="logo-header">
+                        <img className="logo-header-img" src={Logo} alt="Logo da Holding Hands"/>
+
+                        <p className="logo-header-title">
+                            Holding Hands
+                        </p>
+                    </div>
                 </Link>
 
-                <ul className="menu">
-                    <li class="item-menu">
-                        <Link className="link-item-menu" to="/">HOME</Link>
+                <ul className="nav">
+                    <li class="nav-itens">
+                        <Link className="link-nav-itens" to="/">HOME</Link>
                     </li>
                     
-                    <li class="item-menu">
-                        <Link className="link-item-menu" to="/login">LOGIN</Link>
+                    <li class="nav-itens">
+                        <Link className="link-nav-itens" to="/login">LOGIN</Link>
                     </li>
-                    <li class="item-menu">                        
-                        <Link className="link-item-menu" to="/cadastro-de-usuario">CADASTRAR-SE</Link>                        
+                    <li class="nav-itens">                        
+                        <Link className="link-nav-itens" to="/cadastro-de-usuario">CADASTRAR-SE</Link>                        
                     </li>                    
-                    <li class="item-menu">                        
-                        <Link className="link-item-menu" to="/cadastrar-ideia">CADASTRAR IDEIA</Link>                        
+                    <li class="nav-itens">                        
+                        <Link className="link-nav-itens" to="/cadastrar-ideia">CADASTRAR IDEIA</Link>                        
                     </li>
                 </ul>                
             </div>
