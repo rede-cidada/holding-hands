441cf00d18f337706932ddeeead1cf3cc056b2b1
Maria Luisa <malu.olicosta@gmail.com>
2020-10-15 23:50:18 -0300

    Ajustes das rotas dos botões e adição de metas no index.html

--- src/services/routes.js
+++ src/services/routes.js
@@ -14,12 +14,12 @@ import DoacaoDinheiro from '../pages/DoacaoDinheiro';
 const Routes = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="" component={Landing} />
+      <Route exact path="/" component={Landing} />
       <Route path="/lista-de-ideias" component={Ideias} />
       <Route path="/ideia" component={DescricaoIdeia} />
       <Route path="/cadastro-de-usuario" component={CadastroUsuario} />
       <Route path="/doacao-de-objetos" component={DoacaoObjetos} />
-      <Route path="/Login" component={Login} />
+      <Route path="/login" component={Login} />
       <Route path="/cadastrar-ideia" component={CadastroIdeia} />
       <Route path="/doacao-de-dinheiro" component={DoacaoDinheiro} />
     </Switch>
