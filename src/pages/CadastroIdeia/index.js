a1a0c5b806d20a72e507e2cd4ca6a01c11d40535
Maria Luisa <malu.olicosta@gmail.com>
2020-10-16 11:48:32 -0300

    Tela de cadastro de ideia incompleta

--- src/pages/CadastroIdeia/index.js
+++ src/pages/CadastroIdeia/index.js
@@ -7,35 +7,47 @@ const RegisterIdeias = () => {
         <section className = "register-ideia-container">
             <h1 class="register-ideia-title">Cadastro de Ideia</h1>        
             <form className="register-ideia-form" name="registro-idea">      
-                <div className="register-ideia-div" id="Nome-da-organização">
-                    <label className="register-ideia-label" for="Nome-da-organização">Nome:</label>
-                    <input className="registerideia-input-nome register-ideia-input" type="text" id="Nome-da-organização" nome="Nome-da-organização" required/>
-                </div>
-                   
-                <div className="register-ideia-div">
-                    <label className="register-ideia-label" for="endereco">Endereço:</label>
-                    <input className="registerideia-input-endereco register-ideia-input" type="text" id="endereco" name="endereco" required/>
-                </div>
-                    
-                <div className="register-ideia-div">
-                    <label className="register-ideia-label" for="Telefone">Telefone:</label>
-                    <input className="registerideia-input-telefone register-ideia-input" type="number" id="telefone" name="Telefone" maxlength="11" required />
-                </div>
-                    
-                <div className="register-ideia-div">
-                    <label className="register-ideia-label" for="e-mail"> E-mail:</label>
-                    <input className="registerideia-input-email register-ideia-input" type="email" id="email" name="e-mail" required />
-                </div>
-                <div className="registerideia-cidade-uf">   
+                <div class="registerideia-nome-endereco">
+                    <div className="register-ideia-div" id="Nome-da-organização">
+                        <label className="register-ideia-label" for="Nome-da-organização">Nome:</label>
+                        <input className="registerideia-input-nome register-ideia-input" type="text" id="Nome-da-organização" nome="Nome-da-organização" required/>
+                    </div>
+                
                     <div className="register-ideia-div">
+                        <label className="register-ideia-label" for="endereco">Endereço:</label>
+                        <input className="registerideia-input-endereco register-ideia-input" type="text" id="endereco" name="endereco" required/>
+                    </div>
+                </div> 
+
+                <div class="registerideia-nome-endereco">                   
+                    <div className="register-ideia-div">
+                        <label className="register-ideia-label" for="Telefone">Telefone:</label>
+                        <input className="registerideia-input-telefone register-ideia-input" type="number" id="telefone" name="Telefone" maxlength="11" required />
+                    </div>
+                        
+                    <div className="register-ideia-div">
+                        <label className="register-ideia-label" for="e-mail">E-mail:</label>
+                        <input className="registerideia-input-email register-ideia-input" type="email" id="email" name="e-mail" required />
+                    </div>
+                </div>
+
+                <div className="registerideia-cidade-uf"> 
+                    <div className="register-ideia-div register-ideia-location">  
+                        <label className="register-ideia-label" for="Descricao">Descrição:</label>
+                        <textarea className="register-ideia-textarea register-ideia-input" nome="Descricao"></textarea>
+                    </div>   
+                    
+                    <div className="register-ideia-div register-ideia-location">
                         <label className="register-ideia-label" for="cidade">Cidade:</label>
                         <input className="registerideia-input-cidade" type="text" id="cidade" name="cidade" required />
                     </div>
                         
-                    <div className="register-ideia-div">
+                    <div className="register-ideia-div register-ideia-location">
                         <label className="register-ideia-label" for="uf"> UF:</label>
                         <input className="registerideia-input-uf" type="text" id="uf" name="UF" required />
                     </div>
+
+
                 </div> 
                     
                 <div className="register-ideia-div">
@@ -75,14 +87,9 @@ const RegisterIdeias = () => {
                 </div>
 
                 <div className="register-ideia-btn-div">
-                    <button className="link-item-menu" type="submit" onClick={ () => alert('Não foi possível realizar o cadastro') }>Cadastrar</button>
-                    <Link className="link-item-menu" to="/">Voltar</Link>                     
-                </div>
-
-                <div className="register-ideia-div">  
-                    <label className="register-ideia-label" for="Descricao">Descrição:</label>
-                    <textarea className="register-ideia-textarea" nome="Descricao"></textarea>
-                </div>                                                       
+                    <Link className="link-item-menu" to="/">Voltar</Link> 
+                    <button className="link-item-menu" type="submit" onClick={ () => alert('Não foi possível realizar o cadastro') }>Cadastrar</button>                   
+                </div>                                                      
             </form>
         </section>
     )
