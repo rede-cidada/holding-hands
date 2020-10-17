af5740b3207da3da7f72682b73b936152e30f830
Maria Luisa <malu.olicosta@gmail.com>
2020-10-17 11:03:46 -0300

    Header funcionando

--- src/services/routes.js
+++ src/services/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Landing from '../pages/Landing';
+import Landing from '../pages/landing';
 import Ideias from '../pages/Ideias';
 import CadastroUsuario from '../pages/CadastroUsuario';
 import DoacaoObjetos from '../pages/DoacaoObjetos';
@@ -11,17 +11,15 @@ import CadastroIdeia from '../pages/CadastroIdeia';
 import DoacaoDinheiro from '../pages/DoacaoDinheiro';
 
 const Routes = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Landing} />
-      <Route path="/lista-de-ideias" component={Ideias} />
-      <Route path="/cadastro-de-usuario" component={CadastroUsuario} />
-      <Route path="/doacao-de-objetos" component={DoacaoObjetos} />
-      <Route path="/login" component={Login} />
-      <Route path="/cadastrar-ideia" component={CadastroIdeia} />
-      <Route path="/doacao-de-dinheiro" component={DoacaoDinheiro} />
-    </Switch>
-  </BrowserRouter>
+  <Switch>
+    <Route exact path="/" component={Landing} />
+    <Route path="/lista-de-ideias" component={Ideias} />
+    <Route path="/cadastro-de-usuario" component={CadastroUsuario} />
+    <Route path="/doacao-de-objetos" component={DoacaoObjetos} />
+    <Route path="/login" component={Login} />
+    <Route path="/cadastrar-ideia" component={CadastroIdeia} />
+    <Route path="/doacao-de-dinheiro" component={DoacaoDinheiro} />
+  </Switch>
 );
 
 export default Routes;
